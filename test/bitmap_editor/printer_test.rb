c17fe3c4240fb0be1d2f77e55c5822189b09022f
Artem Skripka <therewasadrizzle@gmail.com>
2020-03-07 22:38:23 +0400

    add spec for printer

--- test/bitmap_editor/printer_test.rb
+++ test/bitmap_editor/printer_test.rb
@@ -1,6 +1,12 @@
 require "minitest/autorun"
 
 class PrinterTest < Minitest::Test
-  def test_validate
+  def test_print_canvas
+    canvas = Canvas.new([['O', 'Z'], ['Z', 'O']])
+    out, _ = capture_io do
+      BitmapEditor::Printer.call(canvas)
+    end
+
+    assert { out == "OZ\nZO\n" }
   end
 end
