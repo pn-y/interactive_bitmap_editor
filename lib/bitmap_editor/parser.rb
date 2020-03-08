dccf43b89cec107ae7fa05329548264454188863
Artem Skripka <therewasadrizzle@gmail.com>
2020-03-08 21:28:40 +0400

    Use validated for parser

--- lib/bitmap_editor/parser.rb
+++ lib/bitmap_editor/parser.rb
@@ -23,7 +23,7 @@ class BitmapEditor
     ALLOWED_SIZE_RANGE = (1..250).freeze
 
     class << self
-      include Dry::Monads[:result]
+      include Dry::Monads[:validated, :result]
 
       def call(string_command)
         parse(string_command)
@@ -35,45 +35,45 @@ class BitmapEditor
         values = string_command.split(' ')
         case values
         in [CLEAR_CANVAS]
-          Success(action: COMMANDS[CLEAR_CANVAS][:action])
+          Valid(action: COMMANDS[CLEAR_CANVAS][:action])
         in [PRINT_CANVAS]
-          Success(action: COMMANDS[PRINT_CANVAS][:action])
+          Valid(action: COMMANDS[PRINT_CANVAS][:action])
         in [CREATE_CANVAS, width, height]
           cast_result = cast_to_int([width, height])
           if cast_result.failure?
-            return Failure("Doesn't know how to parse '#{string_command}'")
+            return Invalid("Doesn't know how to parse '#{string_command}'")
           end
 
           width, height = cast_result.success
           unless ALLOWED_SIZE_RANGE.include?(width) && ALLOWED_SIZE_RANGE.include?(height)
-            return Failure("Doesn't know how to parse '#{string_command}'")
+            return Invalid("Doesn't know how to parse '#{string_command}'")
           end
 
-          Success(action: COMMANDS[CREATE_CANVAS][:action], width: width, height: height)
+          Valid(action: COMMANDS[CREATE_CANVAS][:action], width: width, height: height)
         in [COLOR_PIXEL, x_coord, y_coord, String => color] if ALLOWED_COLORS.include?(color)
           cast_result = cast_to_int([x_coord, y_coord])
           if cast_result.failure?
-            return Failure("Doesn't know how to parse '#{string_command}'")
+            return Invalid("Doesn't know how to parse '#{string_command}'")
           end
 
           x_coord, y_coord = cast_result.success
           if !(ALLOWED_SIZE_RANGE.include?(x_coord) && ALLOWED_SIZE_RANGE.include?(y_coord))
-            return Failure("Doesn't know how to parse '#{string_command}'")
+            return Invalid("Doesn't know how to parse '#{string_command}'")
           end
 
-          Success(action: COMMANDS[COLOR_PIXEL][:action], x_coord: x_coord, y_coord: y_coord, color: color)
+          Valid(action: COMMANDS[COLOR_PIXEL][:action], x_coord: x_coord, y_coord: y_coord, color: color)
         in [DRAW_HORIZONTAL_SEGMENT, column_start, column_end, row, String => color] if ALLOWED_COLORS.include?(color) && column_start <= column_end
           cast_result = cast_to_int([column_start, column_end, row])
           if cast_result.failure?
-            return Failure("Doesn't know how to parse '#{string_command}'")
+            return Invalid("Doesn't know how to parse '#{string_command}'")
           end
 
           column_start, column_end, row = cast_result.success
           if !(ALLOWED_SIZE_RANGE.include?(row) && ALLOWED_SIZE_RANGE.include?(column_end) && ALLOWED_SIZE_RANGE.include?(column_start))
-            return Failure("Doesn't know how to parse '#{string_command}'")
+            return Invalid("Doesn't know how to parse '#{string_command}'")
           end
 
-          Success(
+          Valid(
             action: COMMANDS[DRAW_HORIZONTAL_SEGMENT][:action],
             column_start: column_start,
             column_end: column_end,
@@ -83,15 +83,15 @@ class BitmapEditor
         in [DRAW_VERTICAL_SEGMENT, column, row_start, row_end, String => color] if ALLOWED_COLORS.include?(color) && row_start <= row_end
           cast_result = cast_to_int([column, row_start, row_end])
           if cast_result.failure?
-            return Failure("Doesn't know how to parse '#{string_command}'")
+            return Invalid("Doesn't know how to parse '#{string_command}'")
           end
 
           column, row_start, row_end = cast_result.success
           if !(ALLOWED_SIZE_RANGE.include?(column) && ALLOWED_SIZE_RANGE.include?(row_end) && ALLOWED_SIZE_RANGE.include?(row_start))
-            return Failure("Doesn't know how to parse '#{string_command}'")
+            return Invalid("Doesn't know how to parse '#{string_command}'")
           end
 
-          Success(
+          Valid(
             action: COMMANDS[DRAW_VERTICAL_SEGMENT][:action],
             row_start: row_start,
             row_end: row_end,
@@ -99,7 +99,7 @@ class BitmapEditor
             color: color
           )
         else
-          Failure("Doesn't know how to parse line '#{string_command}'")
+          Invalid("Doesn't know how to parse line '#{string_command}'")
         end
       end
 
