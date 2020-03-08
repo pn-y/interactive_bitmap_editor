3ed51e03603023640d2b0022456b60ab70063fab
Artem Skripka <therewasadrizzle@gmail.com>
2020-03-08 04:15:49 +0400

    update integration spec

--- test/bitmap_editor_test.rb
+++ test/bitmap_editor_test.rb
@@ -6,7 +6,14 @@ class BitmapEditorTest < Minitest::Test
   def test_process
     example_path = Dir.pwd + '/test/fixtures/example.txt'
     example_result = File.read(Dir.pwd + '/test/fixtures/example_result.txt')
-    result = BitmapEditor.process(example_path)
-    assert { result ==  example_result }
+    result = BitmapEditor.new.process(example_path)
+    assert { result.to_s == example_result }
+  end
+
+  def test_process_with_clear
+    example_path = Dir.pwd + '/test/fixtures/example_with_clear.txt'
+    example_result = File.read(Dir.pwd + '/test/fixtures/example_with_clear_result.txt')
+    result = BitmapEditor.new.process(example_path)
+    assert { result.to_s == example_result }
   end
 end
